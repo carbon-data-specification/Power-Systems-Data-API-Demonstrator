55de3242a15c0a9fb017cbba4ecfcb5ee9670b8d
Pierre Segonne <pierre.segonne@tmrow.com>
2023-01-05 13:38:27 +0100

    Add control for grid node routes

--- Controllers/SystemController.cs
+++ Controllers/SystemController.cs
@@ -16,6 +16,65 @@ namespace LFEnergy.Controllers
         }
 
         [HttpGet]
+        [Route("/gridNode/list")]
+        public IEnumerable<GridNode> ListGridNodes()
+        {
+            // TODO add filtering by type
+            GridNode sampleGridNode1 = new GridNode()
+            {
+                ID = "nwjefnwm:@",
+                name = "ES",
+                type = GridNodeType.System,
+            };
+            GridNode sampleGridNode2 = new GridNode()
+            {
+                ID = "wpfm[",
+                name = "production_unit_coal",
+                type = GridNodeType.ProductionUnit,
+            };
+
+            return new List<GridNode>() { sampleGridNode1, sampleGridNode2 };
+        }
+
+        [HttpGet]
+        [Route("/gridNode/describe/{id}")]
+        public GridNode DescribeGridNode()
+        {
+            // TODO find matching node
+            GridNode sampleGridNode = new GridNode()
+            {
+                ID = "nwjefnwm:@",
+                name = "ES",
+                type = GridNodeType.System,
+                generation = new Generation()
+                {
+                    PowerPerFuelType = new Dictionary<FuelType, uint>()
+                    {
+                        { FuelType.Solar, 3457 },
+                        { FuelType.Wind, 1650 },
+                        { FuelType.BrownCoal, 432 }
+                    },
+                    Region = "ES",
+                    TimeStamp = DateTime.UtcNow,
+                    TotalPower = 5107
+                },
+                importExport = new ImportExport()
+                {
+                    Region = "ES",
+                    TimeStamp = DateTime.UtcNow,
+                    TotalPower = 534,
+                    PowerPerImport = new Dictionary<string, uint>()
+                    {
+                        { "FR", 534 }
+                    }
+                }
+            };
+
+            return sampleGridNode;
+        }
+
+
+        [HttpGet]
         [Route("/generation")]
         public IEnumerable<Generation> GetGeneration()
         {
--- GridNode.cs
+++ GridNode.cs
@@ -1,51 +1,32 @@
 ﻿namespace LFEnergy
 {
-	public class GridNode
+
+    public class GridNode
 	{
         public string ID { get; set; } = string.Empty;
-        public string Name { get; set; } = string.Empty;
-        public GridNodeType Type { get; set; } = GridNodeType.ProductionUnit;
+        public string name { get; set; } = string.Empty;
+        public GridNodeType type { get; set; } = GridNodeType.ProductionUnit;
+        // Optional static data
+        // Could use inheritance for fields that are specific to only one grid node type
+        public string market { get; set; } = string.Empty;
+        public int startYear { get; set; } = 0;
+        public string ownerInformation { get; set; } = string.Empty;
+        // TODO would be proper type
+        public string location { get; set; } = string.Empty;
+
+        // parenthood relations
+        public List<GridNode> gridNodeParents { get; set; } = new List<GridNode> { };
+        public List<GridNode> gridNodeChildren { get; set; } = new List<GridNode> { };
 
         // We would also need GET all nodes, with possibility to filter per type, name, parent, location etc
-        // 
 
-        // Below does not work probably
-        public class Generation
-        {
-            public DateTime TimeStamp { get; set; } // in UTC
+        public Generation generation { get; set; } = new Generation();
+        // IF GridNodeType is not at system level should return that it can't be queried
+        public Demand demand { get; set; } = new Demand();
+        // IF GridNodeType is not at system level should return that it can't be queried
+        public ImportExport importExport { get; set; } = new ImportExport();
+        public Emissions emissions { get; set; } = new Emissions();
 
-            public string Region { get; set; } = string.Empty;
-
-            public uint TotalPower { get; set; } // in MW
-
-            public Dictionary<FuelType, uint> PowerPerFuelType { get; set; } = new Dictionary<FuelType, uint>(); // in MW
-        }
-
-        public class Demand
-        {
-            // IF GridNodeType is not at system level should return that it can't be queried
-            public DateTime TimeStamp { get; set; } // in UTC
-
-            public string Region { get; set; } = string.Empty;
-
-            public uint TotalPower { get; set; } // in MW
-        }
-
-        public class ImportExport
-        {
-            // IF GridNodeType is not at system level should return that it can't be queried
-            public DateTime TimeStamp { get; set; } // in UTC
-
-            public string Region { get; set; } = string.Empty;
-
-            public uint TotalPower { get; set; } // in MW
-
-            public Dictionary<string, uint> PowerPerImport { get; set; } = new Dictionary<string, uint>(); // in MW
-        }
-
-        public class Emissions
-        {
-            public Dictionary<Pollutant, uint> EmissionsPerPolutant { get; set; } = new Dictionary<Pollutant, uint>(); // in KG
-        }
+    
     }
 }
