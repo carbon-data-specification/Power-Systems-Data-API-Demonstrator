a1e2fc8cbf462381afacd14b52b8702941a571ac
Stephen Suffian <stephen.suffian@pm.me>
2023-03-20 10:12:35 -0400

    Renamed grid node id to id and fixed error with generation/describe endpoints
    Signed-off-by: Stephen Suffian <stephen.suffian@pm.me>

--- power_systems_data_api_demonstrator/src/api/grid_node/views.py
+++ power_systems_data_api_demonstrator/src/api/grid_node/views.py
@@ -1,12 +1,15 @@
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.param_functions import Depends
 
 from power_systems_data_api_demonstrator.src.api.grid_node.schema import (
     GridNodeModelDTO,
 )
-from power_systems_data_api_demonstrator.src.lib.db.dao.grid_node_dao import GridNodeDAO
+from power_systems_data_api_demonstrator.src.lib.db.dao.grid_node_dao import (
+    GridNodeDAO,
+    GridNodeNotFoundError,
+)
 from power_systems_data_api_demonstrator.src.lib.db.models.grid_node_model import (
     GridNodeModel,
 )
@@ -29,9 +32,9 @@ async def list_grid_nodes(
     return await grid_node_dao.get_all_grid_nodes(limit=limit)
 
 
-@router.get("/describe/{grid_node_id}", response_model=GridNodeModelDTO)
+@router.get("/describe/{id}", response_model=GridNodeModelDTO)
 async def describe_grid_nodes(
-    grid_node_id: int,
+    id: str,
     grid_node_dao: GridNodeDAO = Depends(),
 ) -> GridNodeModel:
     """
@@ -41,19 +44,25 @@ async def describe_grid_nodes(
     :param dummy_dao: DAO for grid nodes.
     :return: a single grid node with the given id.
     """
-    return await grid_node_dao.get_by_id(grid_node_id)
+    try:
+        return await grid_node_dao.get_by_id(id)
+    except GridNodeNotFoundError as exc:
+        raise HTTPException(status_code=404, detail=str(exc)) from None
 
 
-@router.get("/generation/{grid_node_id}", response_model=GridNodeModelDTO)
+@router.get("/generation/{id}", response_model=GridNodeModelDTO)
 async def get_generation_grid_node(
-    grid_node_id: int,
+    id: str,
     grid_node_dao: GridNodeDAO = Depends(),
 ) -> GridNodeModel:
     """
-    Retrieve a description of a single grid node.
+    Retrieve generation data for a single grid node.
 
     :param id: id of a specific grid node.
     :param dummy_dao: DAO for grid nodes.
     :return: a single grid node with the given id.
     """
-    return await grid_node_dao.get_by_id(grid_node_id)
+    try:
+        return await grid_node_dao.get_by_id(id)
+    except GridNodeNotFoundError as exc:
+        raise HTTPException(status_code=404, detail=str(exc)) from None
