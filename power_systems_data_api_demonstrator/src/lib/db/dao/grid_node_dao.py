355961afb09514312a402fad1449fc155f82920e
Pierre Segonne <pierre.segonne@tmrow.com>
2023-04-17 18:44:51 +0200

    Add logic for serving capacity
    Signed-off-by: Pierre Segonne <pierre.segonne@tmrow.com>

--- power_systems_data_api_demonstrator/src/lib/db/dao/grid_node_dao.py
+++ power_systems_data_api_demonstrator/src/lib/db/dao/grid_node_dao.py
@@ -11,6 +11,7 @@ from power_systems_data_api_demonstrator.src.lib.db.models.exchanges import (
     ExchangeModel,
 )
 from power_systems_data_api_demonstrator.src.lib.db.models.generation import (
+    CapacityForFuelTypeModel,
     GenerationForFuelTypeModel,
 )
 from power_systems_data_api_demonstrator.src.lib.db.models.grid_node_model import (
@@ -130,6 +131,18 @@ class GridNodeDAO:
         )
         exports = list(raw_exports.scalars().fetchall())
         return imports + exports
+
+    async def get_capacity(self, grid_node_id: str) -> list[CapacityForFuelTypeModel]:
+        """
+        Get capacity of a grid node.
+        """
+        raw_capacity = await self.session.execute(
+            select(CapacityForFuelTypeModel).filter(
+                CapacityForFuelTypeModel.grid_node_id == grid_node_id
+            ),
+        )
+        capacity = list(raw_capacity.scalars().fetchall())
+        return capacity
 
     async def get_all_grid_nodes(self, limit: int | None = None) -> List[GridNodeModel]:
         """
