170d21acb82745813b24ca9ca56c43f451415a75
Pierre Segonne <pierre.segonne@tmrow.com>
2023-04-18 14:00:13 +0200

    Fix routes documentation
    Signed-off-by: Pierre Segonne <pierre.segonne@tmrow.com>

--- power_systems_data_api_demonstrator/src/api/grid_node/views.py
+++ power_systems_data_api_demonstrator/src/api/grid_node/views.py
@@ -32,51 +32,42 @@ from power_systems_data_api_demonstrator.src.lib.db.models.prices import (
 router = APIRouter()
 
 
-@router.get("/list", response_model=List[GridNodeModelDTO])
+@router.get(
+    "/list",
+    response_model=List[GridNodeModelDTO],
+    summary="List all available grid nodes",
+)
 async def list_grid_nodes(
     limit: int = 10,
     grid_node_dao: GridNodeDAO = Depends(),
 ) -> List[GridNodeModel]:
-    """
-    Retrieve all grid nodes from the database.
-
-    :param limit: limit of grid nodes, defaults to 10.
-    :param grid_node_dao: DAO for grid nodes.
-    :return: list of grid nodes from database.
-    """
     return await grid_node_dao.get_all_grid_nodes(limit=limit)
 
 
-@router.get("/describe/{id}", response_model=GridNodeModelDTO)
+@router.get(
+    "/describe/{id}",
+    response_model=GridNodeModelDTO,
+    summary="Describe a given grid node",
+)
 async def describe_grid_nodes(
     id: str,
     grid_node_dao: GridNodeDAO = Depends(),
 ) -> GridNodeModel:
-    """
-    Retrieve a description of a single grid node.
-
-    :param id: id of a specific grid node.
-    :param dummy_dao: DAO for grid nodes.
-    :return: a single grid node with the given id.
-    """
     try:
         return await grid_node_dao.get_by_id(id)
     except GridNodeNotFoundError as exc:
         raise HTTPException(status_code=404, detail=str(exc)) from None
 
 
-@router.get("/capacity/{id}", response_model=list[CapacityDTO])
+@router.get(
+    "/capacity/{id}",
+    response_model=list[CapacityDTO],
+    summary="Get installed generation capacity for a grid node",
+)
 async def get_capacity_grid_node(
     id: str,
     grid_node_dao: GridNodeDAO = Depends(),
 ) -> list[CapacityDTO]:
-    """
-    Retrieve generation data for a single grid node.
-
-    :param id: id of a specific grid node.
-    :param dummy_dao: DAO for grid nodes.
-    :return: a single grid node with the given id.
-    """
     try:
         raw_capacities = await grid_node_dao.get_capacity(id)
         capacity = []
@@ -101,92 +92,73 @@ async def get_capacity_grid_node(
         raise HTTPException(status_code=404, detail=str(exc)) from None
 
 
-# TODO define generation model and return
-@router.get("/generation/{id}", response_model=list[GenerationDTO])
+@router.get(
+    "/generation/{id}",
+    response_model=list[GenerationDTO],
+    summary="Get generation for a grid node",
+)
 async def get_generation_grid_node(
     id: str,
     grid_node_dao: GridNodeDAO = Depends(),
 ) -> list[GenerationDTO]:
-    """
-    Retrieve generation data for a single grid node.
-
-    :param id: id of a specific grid node.
-    :param dummy_dao: DAO for grid nodes.
-    :return: a single grid node with the given id.
-    """
     try:
         return await grid_node_dao.get_generation(id)
     except GridNodeNotFoundError as exc:
         raise HTTPException(status_code=404, detail=str(exc)) from None
 
 
-# TODO define demand model and return
-@router.get("/demand/{id}", response_model=list[DemandDTO])
+@router.get(
+    "/demand/{id}", response_model=list[DemandDTO], summary="Get demand for a grid node"
+)
 async def get_demand_grid_node(
     id: str,
     grid_node_dao: GridNodeDAO = Depends(),
 ) -> list[DemandModel]:
-    """
-    Retrieve demand data for a single grid node.
-
-    :param id: id of a specific grid node.
-    :param grid_node_dao: DAO for grid nodes.
-    :return: a single grid node with the given id.
-    """
     try:
         return await grid_node_dao.get_demand(id)
     except GridNodeNotFoundError as exc:
         raise HTTPException(status_code=404, detail=str(exc)) from None
 
 
-@router.get("/dayAheadPrice/{id}", response_model=List[DayAheadPriceDTO])
+@router.get(
+    "/dayAheadPrice/{id}",
+    response_model=List[DayAheadPriceDTO],
+    summary="Get day ahead price for a grid node",
+)
 async def get_day_ahead_price_grid_node(
     id: str,
     grid_node_dao: GridNodeDAO = Depends(),
 ) -> List[DayAheadPriceModel]:
-    """
-    Retrieve demand data for a single grid node.
-
-    :param id: id of a specific grid node.
-    :param dummy_dao: DAO for grid nodes.
-    :return: a single grid node with the given id.
-    """
     try:
         return await grid_node_dao.get_day_ahead_price(id)
     except GridNodeNotFoundError as exc:
         raise HTTPException(status_code=404, detail=str(exc)) from None
 
 
-@router.get("/imports/{id}", response_model=list[ExchangeDTO])
+@router.get(
+    "/imports/{id}",
+    response_model=list[ExchangeDTO],
+    summary="Get imports for a grid node",
+)
 async def get_imports_grid_node(
     id: str,
     grid_node_dao: GridNodeDAO = Depends(),
 ) -> list[ExchangeModel]:
-    """
-    Retrieve generation data for a single grid node.
-
-    :param id: id of a specific grid node.
-    :param dummy_dao: DAO for grid nodes.
-    :return: a single grid node with the given id.
-    """
     try:
         return await grid_node_dao.get_imports(id)
     except GridNodeNotFoundError as exc:
         raise HTTPException(status_code=404, detail=str(exc)) from None
 
 
-@router.get("/exports/{id}", response_model=list[ExchangeDTO])
+@router.get(
+    "/exports/{id}",
+    response_model=list[ExchangeDTO],
+    summary="Get exports for a grid node",
+)
 async def get_exports_grid_node(
     id: str,
     grid_node_dao: GridNodeDAO = Depends(),
 ) -> list[ExchangeModel]:
-    """
-    Retrieve generation data for a single grid node.
-
-    :param id: id of a specific grid node.
-    :param dummy_dao: DAO for grid nodes.
-    :return: a single grid node with the given id.
-    """
     try:
         return await grid_node_dao.get_exports(id)
     except GridNodeNotFoundError as exc:
