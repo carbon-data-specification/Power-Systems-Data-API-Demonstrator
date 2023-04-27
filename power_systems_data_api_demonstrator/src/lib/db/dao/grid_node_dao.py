d5cc0cc72289fb00540495b64fac8fc4a1e6bbf5
Stephen Suffian <stephen.suffian@pm.me>
2023-04-27 11:23:21 -0400

    Updated seeder and seed to get tests to pass
    Signed-off-by: Stephen Suffian <stephen.suffian@pm.me>

--- power_systems_data_api_demonstrator/src/lib/db/dao/grid_node_dao.py
+++ power_systems_data_api_demonstrator/src/lib/db/dao/grid_node_dao.py
@@ -59,16 +59,16 @@ class GridNodeDAO:
     def __init__(self, session: AsyncSession = Depends(get_db_session)):
         self.session = session
 
-    async def create_grid_node(
-        self, grid_node_model: GridNodeModel, overwrite_if_exists: bool = True
-    ) -> None:
+    async def delete_all_grid_nodes(self) -> None:
+        delete_stmt = delete(GridNodeModel)
+        await self.session.execute(delete_stmt)
+        await self.session.commit()
+
+    async def create_grid_node(self, grid_node_model: GridNodeModel) -> None:
         """
         Add single grid_node to session.
         :param name: name of a grid_node.
         """
-        if overwrite_if_exists:
-            delete_stmt = delete(GridNodeModel).filter_by(id=grid_node_model.id)
-            await self.session.execute(delete_stmt)
         self.session.add(grid_node_model)
         await self.session.commit()
 
@@ -272,6 +272,16 @@ class GridNodeDAO:
         if not value:
             raise GridNodeNotFoundError(id)
         return value
+
+    async def check_if_grid_node_exists(self, id: str) -> bool:
+        """
+        Get specific grid_node model.
+        :param name: name of grid_node instance.
+        :return: grid_node models.
+        """
+        query = select(GridNodeModel).where(GridNodeModel.id == id)
+        rows = await self.session.execute(query)
+        return rows.scalars().one_or_none() is not None
 
     async def delete_grid_node(self, id: str) -> None:
         delete_stmt = delete(GridNodeModel).filter_by(id=id)
