5042808e2b191831fd7ed22de2c3397e3ebec5b3
Andre <andberna@outlook.es>
2023-11-20 18:26:16 +0100

    Add PSR list to the metadata
    Signed-off-by: Andre <andberna@outlook.es>

--- power_systems_data_api_demonstrator/src/api/psr_metadata/views.py
+++ power_systems_data_api_demonstrator/src/api/psr_metadata/views.py
@@ -1,30 +1,39 @@
-from pydantic import BaseModel
-from sqlmodel import select
+from typing import Annotated, Optional
 
-from sqlmodel import Session
-from enum import Enum
-from sqlmodel import Field
-from datetime import datetime
-from sqlmodel import SQLModel
-
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Path
 from fastapi.param_functions import Depends
-from power_systems_data_api_demonstrator.src.api.db import get_session
+from sqlmodel import Field, Session, SQLModel, select
 
+from power_systems_data_api_demonstrator.src.api.db import get_session
 
 router = APIRouter()
 
 
-class DescribeResponse(BaseModel):
-    pass
+class PSRList(SQLModel, table=True):
+    id: str = Field(primary_key=True)
+    level: int
+    name: Optional[str]
+
+
+class PSRListResponse(SQLModel):
+    psr_list: list[PSRList]
+
+
+class PSRListReqest(SQLModel):
+    level: int
 
 
 @router.get(
-    "/{id}/describe",
-    summary="TOPOLOGY DESCRIPTION",
+    "/power-system-resources",
+    summary="PSR LIST",
 )
-async def get_topology_levels(
-    id: int,
+async def get_psr_list(
+    level: Annotated[Optional[int], Path(description="Filter by level")] | None = None,
     session: Session = Depends(get_session),
-) -> DescribeResponse:
-    return {}
+) -> PSRListResponse:
+    if level is None:
+        result = session.execute(select(PSRList))
+    else:
+        result = session.execute(select(PSRList).filter_by(level=level))
+    psr_list = result.scalars().all()
+    return PSRListResponse(psr_list=psr_list)
